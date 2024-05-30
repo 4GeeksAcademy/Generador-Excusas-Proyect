776839231d1cad510b72dac2f209c8f47628e078
Bryan <168112800+BryanBV03@users.noreply.github.com>
2024-05-30 18:33:51 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const generarExcusa = () => {
+    let sujetos = ["Mi perro", "El vecino", "Un alien", "Mi abuela", "El gato"];
+
+    let Acciones = ["comió", "rompió", "robó", "perdió", "escondió"];
+
+    let complementos = [
+      "mi tarea",
+      "las llaves",
+      "mi cartera",
+      "el coche",
+      "mis zapatos"
+    ];
+
+    let sujetoAleatorio = sujetos[Math.floor(Math.random() * sujetos.length)];
+    let AccionesAleatorio =
+      Acciones[Math.floor(Math.random() * Acciones.length)];
+    let complementoAleatorio =
+      complementos[Math.floor(Math.random() * complementos.length)];
+
+    return `${sujetoAleatorio} ${AccionesAleatorio} ${complementoAleatorio}.`;
+  };
+
+  document.querySelector("#excusa").innerHTML = generarExcusa();
 };
